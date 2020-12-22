971bdd0d70704503314aa00285d67d1a4fa862de
vittorio.cortosi <73166428+cortosi@users.noreply.github.com>
2020-12-22 21:45:09 +0100

    .

--- Unify/Js/main.js
+++ Unify/Js/main.js
@@ -1,4 +1,4 @@
-var songs = ["/unify/test/Hey Brother.mp3"];
+var songs = ["./test/Hey Brother.mp3"];
 
 var song = new Audio();
 var currentSong = 0;
@@ -20,12 +20,29 @@ $(".playback_play_btn").click(function playOrPauseSong() {
 
 $(".songs_btn").click(function() {
     $(".loaded").empty();
-    $.getJSON("/unify/php/ajax_requests.php", "type=songs", function(json) {
+    $.getJSON("./php/ajax_requests.php", "type=songs", function(json) {
         json.songs.forEach(function(item) {
             var $wrapper = $('<div>', { 'class': 'row_song' });
-            $('<div>', {
+            var $songname = $('<div>', {
                 'class': 'row_song_songname',
-                'html': item.name,
+                'prepend': $('<img>', {
+                    'class': 'song_illustration',
+                    'src': './unify_media/' + item.artist + '/' + item.album + '/cover.jpg',
+                }).click(prova),
+                'append': item.name,
+            });
+            $songname.appendTo($wrapper);
+            $('<div>', {
+                'class': 'row_song_songartist',
+                'html': item.artist,
+            }).appendTo($wrapper);
+            $('<div>', {
+                'class': 'row_song_songalbum',
+                'html': item.album,
+            }).appendTo($wrapper);
+            $('<div>', {
+                'class': 'row_song_songtime',
+                'html': item.lenght,
             }).appendTo($wrapper);
             $wrapper.appendTo(".loaded");
         });
@@ -46,3 +63,7 @@ $(".recently_btn").click(function() {
     $(".loaded").empty();
 
 });
+
+function prova() {
+    console.log($(this).next().text());
+};
--- Unify/php/ajax_requests.php
+++ Unify/php/ajax_requests.php
@@ -3,15 +3,21 @@
     try{
         switch($_GET['type']){
             case 'songs':
-                echo  "{\n\"songs\":[\n";
-                $rows = $db -> prepare("SELECT album.foto,canzone.titolo,album.nome,canzone.durata
-                                        FROM canz_alb
-                                        JOIN canzone ON(canz_alb.id_canzone = canzone.id)
-                                        JOIN album ON(canz_alb.id_album = album.id);");
+                $rows = $db -> prepare("SELECT song.title, artist.name as artname, album.name as albname, song.lenght
+                                        FROM song_alb
+                                        JOIN song ON(song_alb.song_id = song.id)
+                                        JOIN album ON(song_alb.album_id = album.id)
+                                        JOIN song_art ON(song_art.song_id = song.id)
+                                        JOIN artist ON (song_art.artist_id = artist.id);");
                 $rows -> execute();
                 $result = $rows->fetchAll();
+                echo  "{\n\"songs\":[\n";
                 for($i = 0; $i < $rows -> rowCount(); $i++){
-                    echo "{\"name\":\"".$result[$i]['titolo']."\"}";
+                    echo "{\"name\":\"".$result[$i]['title']."\",
+                            \n \"artist\":\"".$result[$i]['artname']."\",
+                            \n \"album\":\"".$result[$i]['albname']."\",
+                            \n \"lenght\":\"".$result[$i]['lenght']."\"
+                    }";
                     if($i != $rows -> rowCount() -1){
                         echo ",\n";
                     }
